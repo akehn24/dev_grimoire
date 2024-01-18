7e82ef36f4b9058c13e1056ffffae258049885d0
Annie Kehn <ajkehn42@gmail.com>
2024-01-17 16:07:56 -0800

    Updated the README with the shortest description in the world and a small TODO to keep track of things.
    Added more to the Python String Manipulation file.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # dev_grimoire
+Somewhere to put every little thing I learn about programming, from the basics to the not-so-basics. Also using this to get more comfortable with git.
+
+TODO:
+- git how to's - transfer and clean from work cookbook
+- python lambda
+- python loops
+- python data structures
--- python/string_manipulation.py
+++ python/string_manipulation.py
@@ -1,12 +1,57 @@
 ############################################################################
 # String Manipulation in Python
 # 
-# Removing Leading/Trailing Characters
-# Print First/Last Characters
-# Capitalize and Lower Case
+# TOC:
+#   Strings are Arrays
+#   Multiline Strings
+#   Removing Leading/Trailing Characters
+#   Capitalize and Lower Case
+#   Count Length
+#   Start and End of Strings
+#   Character Replacement
+#   Concatenating Strings
+#   Escape Characters
+# 
+# Credits:
+#   Nicole van der Hoeven's Fork My Brain: https://notes.nicolevanderhoeven.com/Fork+My+Brain
+#   W3Schools: https://www.w3schools.com/python/python_strings.asp
+#   W3Schools List of String Methods: https://www.w3schools.com/python/python_ref_string.asp
 ############################################################################
 
-print()
+print("\nStrings are Arrays:")
+### Strings are Arrays ######################################################
+string_array = "Strings can be indexed."
+print(f"{string_array}: {string_array[1]}")
+print(f"{string_array}: {string_array[0:5]}")
+print(f"{string_array}: {string_array[5:10]}")
+print(f"{string_array} sliced from the start: {string_array[:10]}")
+print(f"{string_array} sliced from the end: {string_array[15:]}")
+print(f"{string_array} sliced from the start in reverse: {string_array[-10:]}")
+print(f"{string_array} sliced from the end in reverse: {string_array[:-5]}")
+
+string_array = "Loop"
+for char in string_array:
+    print(char)
+
+# check if substring in string
+if "oo" in string_array:
+    print(f"There are double o's in {string_array}.")
+if "ii" not in string_array:
+    print(f"There are not double i's in {string_array}.")
+############################################################################
+
+
+print("\nMultiline Strings:")
+### Multiline Strings ######################################################
+multiline_string = """Use three
+double or single quotes
+to make multiline
+strings."""
+print(multiline_string)
+############################################################################
+
+
+print("\nRemoving Leading/Trailing Characters:")
 ### Removing Leading/Trailing Characters ###################################
 lead_str = "     Removed the leading spaces."
 lead_str = lead_str.lstrip(" ")
@@ -16,15 +61,13 @@ trail_str = "Removed the trailing spaces.   "
 trail_str = trail_str.rstrip(" ")
 print(trail_str)
 
-print()
-### Print First/Last Characters ###########################################
-manip_string = "Parasaurolophus"
-print(manip_string[3:])
-print(manip_string[6:])
-print(manip_string[:3])
-print(manip_string[:6])
+both_ends = "    Removed the extra spaces from both ends.     "
+both_ends = both_ends.strip()
+print(both_ends)
+############################################################################
 
-print()
+
+print("\nCapitalize and Lower Case:")
 ### Capitalize and Lower Case ##############################################
 # Caps
 all_caps = "capitalize this string"
@@ -49,8 +92,10 @@ print("Broken:", apos_broken)
 apos_fixed = "Message's is fixed's"
 apos_fixed = apos_fixed.title().replace("'S", "'s")
 print("Fixed:", apos_fixed)
+############################################################################
 
-print()
+
+print("\nCount Length:")
 ### Count Length ###########################################################
 city_in_wales = "Llanfairpwllgwyngyllgogerychwyrndrobwllllantysiliogogogoch"
 print(f"The length of {city_in_wales}: {len(city_in_wales)}")
@@ -58,6 +103,60 @@ print(f"The length of {city_in_wales}: {len(city_in_wales)}")
 num_of_spaces = "           How many spaces were in the lead?"
 no_spaces = num_of_spaces.lstrip(" ")
 print(f"{no_spaces} There were {len(num_of_spaces) - len(no_spaces)}.")
+############################################################################
 
-print()
+print("\nStart and End of Strings:")
+### Start and End of Strings ###############################################
+check_start_end = "Does this string start with a D?"
+if check_start_end.startswith("D"):
+    print("String starts with the letter D.")
+
+if check_start_end.endswith("A") == False:
+    print("String does not end with an A.")
+
+range_string = "Parasaurolophus"
+print(f"{range_string[0:8]} ends with saur: {range_string.endswith('saur', 0, 8)}")
+print(f"{range_string[8:15]} ends with saur: {range_string.endswith('saur', 8, 15)}")
+############################################################################
+
+
+print("\nCharacter Replacement:")
+### Character Replacement ##################################################
+dino_string = "Parasaurolophus"
+print(dino_string)
+firsthalf_dino = dino_string.replace("Para", "Tyrana")
+print(firsthalf_dino)
+secondhalf_dino = firsthalf_dino.replace("olophus", "us-Rex")
+print(secondhalf_dino)
+############################################################################
+
+
+print("\nConcatenating Strings:")
+### Concatenating Strings ##################################################
+print("Para" + "saur")
+print("Para" + " " + "saur")
+print("Para", "saur")
+print("Para" + str(23), "saur")
+para = "Para"
+saur = "saur"
+num = 23
+print(f"There are {num} {para}{saur} at my school.")   # this is more secure than format
+print("{0} is a {1} year old {2}.".format(para, num, saur))
+############################################################################
+
+
+print("\nEscape Characters:")
+### Escape Characters ######################################################
+print("""This is how you use \"double quotes\"
+      in your multiline string.""")
+escape_chars = """
+\'  single quote
+\"  double quote
+\n  newline
+\\  backslash
+\r  carriage return
+\t  tab
+\b  backspace
+\f  form feed"""
+print(escape_chars)
 ############################################################################
